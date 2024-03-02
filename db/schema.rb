c26bb3dd1ce3b829106eae4987bcf43d769d068f
Margaux Divernois <margaux.divernois@he-arc.ch>
2024-03-02 13:59:26 +0100

    Add image to character

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_25_162050) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_02_121203) do
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "character_fights", force: :cascade do |t|
     t.integer "character_id", null: false
     t.integer "fight_id", null: false
@@ -42,6 +70,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_25_162050) do
     t.index ["current_player_id"], name: "index_fights_on_current_player_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "character_fights", "characters"
   add_foreign_key "character_fights", "fights"
   add_foreign_key "fights", "character_fights", column: "current_player_id"
